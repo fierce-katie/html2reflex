04971592d5afcb95138d62111411ae3b9ae4617c
Catherine Galkina <catherine@typeable.io>
2024-03-26 13:00:58 +0100

    Input in cycle

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,14 @@ import HtmlReflex (htmlToReflex)
 import System.IO
 
 main :: IO ()
-main = do
-  putStrLn "Paste raw HTML:"
-  s <- getContents'
-  putStrLn $ "Result:\n" <> (T.unpack . htmlToReflex . T.pack $ s)
+main = go
+  where
+    go = do
+      putStrLn "Paste raw HTML and press <Ctrl-D>, press <Ctrl-C> to exit:"
+      tty <- openFile "/dev/tty" ReadMode
+      s <- hGetContents' tty
+      hClose tty
+      putStrLn $ "\n" <> Prelude.replicate 80 '='
+      putStrLn $ "\nResult:\n\n" <> (T.unpack . htmlToReflex . T.pack $ s)
+      putStrLn $ Prelude.replicate 80 '='
+      go
