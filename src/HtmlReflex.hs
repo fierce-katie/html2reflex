f3e8aac5a0fa25ad118b97b077baddc7560dba3a
Catherine Galkina <catherine@typeable.io>
2024-03-26 15:35:16 +0100

    Optional init value

--- src/HtmlReflex.hs
+++ src/HtmlReflex.hs
@@ -14,9 +14,9 @@ htmlToReflex = T.concat . P.map (formatTree 0) . parseTree
 
 formatTree :: Int -> TagTree Text -> Text
 formatTree n = \case
-  TagLeaf (formatTag -> tag) | not (T.null tag) -> shift n <> tag <> "\n"
+  TagLeaf (formatTag n -> tag) | not (T.null tag) -> shift n <> tag <> "\n"
   TagBranch el attrs inner -> T.concat $
-    [ shift n, T.concat $ formatElem el attrs, " "
+    [ shift n, T.concat $ formatElem n el attrs, " "
     , innerFunc inner, "\n", formatInner inner]
   _ -> ""
   where
@@ -28,8 +28,8 @@ formatTree n = \case
       | P.null inner = "blank"
       | otherwise = T.concat $ P.map (formatTree (n+1)) inner
 
-formatElem :: (Eq a, Show a, IsString a) => a -> [Attribute Text] -> [Text]
-formatElem el attrs
+formatElem :: (Eq a, Show a, IsString a) => Int -> a -> [Attribute Text] -> [Text]
+formatElem n el attrs
   | el == "input" = elemWithConfig "input"
   | el == "select" = elemWithConfig "select"
   | el == "textarea" = elemWithConfig "textArea"
@@ -40,15 +40,18 @@ formatElem el attrs
   | otherwise = ["elAttr ", toText el, " ", formatAttrs attrs]
   where
     elemWithConfig e =
-      [e <> "Element (def & initialAttributes .~ ", formatAttrs attrs, ")"]
+      [ e, "Element (def\n"
+      , shift (n+1), "& initialAttributes .~ ", formatAttrs attrs, "\n"
+      , shift (n+1), "-- & ", e, "ElementConfig_initialValue .~ _\n"
+      , shift n, ")"]
 
 toText :: Show a => a -> Text
 toText = T.pack . show
 
-formatTag :: Tag Text -> Text
-formatTag = \case
+formatTag :: Int -> Tag Text -> Text
+formatTag n = \case
   TagText (T.strip -> txt) | not (T.null txt) -> "text \"" <> txt <> "\""
-  TagOpen el attrs -> T.concat $ formatElem el attrs
+  TagOpen el attrs -> T.concat $ formatElem n el attrs
   _ -> ""
 
 shift :: Int -> Text
